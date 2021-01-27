d69a47d2c03432a5afc8bea7f019f8eceeae9a9b
Mats Antonsson <matan848@hotmail.com>
2021-01-27 10:46:11 +0100

    Add project files.

---
+++ OOP/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Person
+    {
+        #region Private fields
+        private int age = 1;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+        #endregion
+
+        #region Public properties
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age must be greater than zero.");
+                }
+                age = value;
+
+            }
+        }
+        public string FName 
+        {
+            get
+            {
+                return fName;
+            }
+            set
+            {
+                if (value.Length < 2 || value.Length > 10)
+                {
+                    throw new ArgumentException("FName is too short or too long.");
+                }
+                fName = value;
+            }
+        }
+        public string LName 
+        {
+            get
+            {
+                return lName;
+            }
+            set
+            {
+                if (value.Length < 3 || value.Length > 15)
+                {
+                    throw new ArgumentException("LName is too short or too long.");
+                }
+                lName = value;
+            }
+        }
+        public double Height { get => height; set => height = value; }
+        public double Weight { get => weight; set => weight = value; }
+        #endregion
+
+        #region Constructors
+        public Person(string firstName, string lastName)
+        {
+            FName = firstName;
+            LName = lastName;
+        }
+        #endregion
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿// Lexicon NA21 - Övning 3 Inkapsling, arv och polymorfism
+using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skapar Nisse Hult");
+            Person person = new Person("Nisse", "Hult");
+            person.Age = 39;
+            Console.WriteLine($"{person.FName} {person.LName} är {person.Age} år gammal.");
+
+            try
+            {
+                Console.WriteLine("Skapar p1");
+                Person p1 = new Person("Bo", "Ek");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Fel vid skapande av Person p1: {ex.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine($"Sätter ålder på {person.FName}");
+                person.Age = 0;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Fel ålder: {ex.Message}");
+            }
+        }
+    }
+}
